2fe2d6891c7cd48b79aacd315c50db712182519b
KARTHIK <151136763+ssk9293@users.noreply.github.com>
2025-06-11 02:34:02 -0700

    resume update

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "../Components/Navbar";
 import { motion } from "framer-motion";
 import { Github, Linkedin, Dribbble } from "lucide-react";
 const ProjectDetail = () => {
